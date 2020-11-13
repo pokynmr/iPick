a94fa7ecaa97ec859456db95b9b9c070d80f097b
Mehdi Rahimi <mehdi.rahimi@ucdenver.edu>
2020-11-13 16:40:44 -0600

    cross checking

--- xcheck.py
+++ xcheck.py
@@ -20,6 +20,8 @@ else:
 import sparky
 import sputil
 import tkutil
+from itertools import combinations
+import collections
 
 
 class xcheck_dialog(tkutil.Dialog, tkutil.Stoppable):
@@ -77,15 +79,15 @@ class xcheck_dialog(tkutil.Dialog, tkutil.Stoppable):
     tol_frm = tk.Frame(btmfrm)
     tol_frm.pack(side='top', fill='both', expand=1)
 
-    self.tol_H = tkutil.entry_field(tol_frm, '1H: ', width=5, initial='1')
+    self.tol_H = tkutil.entry_field(tol_frm, '1H: ', width=5, initial='0.1')
     self.tol_H.frame.pack(side='left', padx=(20,10))
     #tkutil.create_hint(self.tol_H.frame, 'Maximum ')
 
-    self.tol_C = tkutil.entry_field(tol_frm, '13C:', width=5, initial='1')
+    self.tol_C = tkutil.entry_field(tol_frm, '13C:', width=5, initial='0.1')
     self.tol_C.frame.pack(side='left', padx=(5,10))
     #tkutil.create_hint(self.tol_C.frame, 'Maximum ')
 
-    self.tol_N = tkutil.entry_field(tol_frm, '15N:', width=5, initial='1')
+    self.tol_N = tkutil.entry_field(tol_frm, '15N:', width=5, initial='0.1')
     self.tol_N.frame.pack(side='left', padx=(5,10))
     #tkutil.create_hint(self.tol_N.frame, 'Maximum ')
 
@@ -102,14 +104,27 @@ class xcheck_dialog(tkutil.Dialog, tkutil.Stoppable):
     exclude_frm.pack(side='top', fill='both', expand=1, pady=(5,0))
 
 
-    self.exclude_from = tkutil.entry_field(exclude_frm, 'From: ', width=5, initial='1')
+    self.exclude_from = tkutil.entry_field(exclude_frm, 'From: ', width=5, initial='4.7')
     self.exclude_from.frame.pack(side='left', padx=(25,0))
     #tkutil.create_hint(self.exclude_from.frame, 'Maximum ')
 
 
-    self.exclude_to = tkutil.entry_field(exclude_frm, 'To: ', width=5, initial='1')
+    self.exclude_to = tkutil.entry_field(exclude_frm, 'To: ', width=5, initial='4.9')
     self.exclude_to.frame.pack(side='left', padx=(5,10))
     #tkutil.create_hint(self.exclude_to.frame, 'Maximum ')
+
+
+# checkbox
+
+    checkbox_frm = tk.Frame(btmfrm)
+    checkbox_frm.pack(side='top', fill='both', expand=1, pady=(15,0))
+
+    self.note_append = tk.BooleanVar()
+    self.note_append.set(True)
+    checkbox_note_append = tk.Checkbutton(checkbox_frm, highlightthickness=0, text='Append to Note',
+                                            variable=self.note_append)
+    checkbox_note_append.pack(side='top', anchor='w', padx=(0,0))
+    tkutil.create_hint(checkbox_note_append, 'If checked, the result will be appended to the Note property of each peak. If unchecked, the Note will be OVERWRITTEN!')
 
 
 # run button & status
@@ -142,9 +157,11 @@ class xcheck_dialog(tkutil.Dialog, tkutil.Stoppable):
 
   def select_all(self):
     self.spectra_list.listbox.select_set(0, tk.END)
+    self.spectra_selected()
 
     self.status.config(text="Status: Selection is complete.")
     self.status.update()
+
 
 
   def update_list(self):
@@ -187,43 +204,113 @@ class xcheck_dialog(tkutil.Dialog, tkutil.Stoppable):
 
   def run_xcheck(self, *args):
 
-#    print self.specs_nuclei
+    self.status.config(text="Status: Running ...")
+    self.status.update()
+
   # specs_peaks[each_selected_spec] --> all peaks in that spec
+  # specs_nuclei[i] -- > i.e.  ('15N', '13C', '1H')  or  ('13C', '1H', '1H')
 
-    #for spec_peaks in self.specs_peaks:
-#    for spec_nuclei in self.specs_nuclei[0]:
+    num_of_specs = len(self.specs_peaks)
+    combinations_list = list(combinations(range(num_of_specs), 2))
+    # list(combinations(range(3), 2))  -- >  [(0, 1), (0, 2), (1, 2)]
+    # 8 spec is 28 combinations!
 
-#        print spec_nuclei
+    for spec in self.specs_peaks:
+        for peak in spec:
+            if self.note_append.get():
+                peak.note += ';xcheck:'
+            else:
+                peak.note = 'xcheck:'
+
+    for spec_pair in combinations_list:
+        s1 = spec_pair[0]
+        s2 = spec_pair[1]
+
+        print("Comparing " + self.specs_names[s1] + " with " + self.specs_names[s2] + ":")
+
+        if (self.specs_nuclei[s1][0] != self.specs_nuclei[s2][0]):
+            print("Will not compare a N-HSQC based experiments with a C-HSQC one")
+            print('_' * 50)
+            continue
+
+        tol = float(self.tol_C.variable.get())
+        if self.specs_nuclei[s1][0] == '15N':
+            tol = float(self.tol_N.variable.get())
+
+        tol_H = float(self.tol_H.variable.get())
+        exclude_from = float(self.exclude_from.variable.get())
+        exclude_to = float(self.exclude_to.variable.get())
+
+        print('Total peaks in the first one: ' + str(len(self.specs_peaks[s1])))
+        print('Total peaks in the second one: ' + str(len(self.specs_peaks[s2])))
+
+        for i, peak1 in enumerate(self.specs_peaks[s1]):
+
+            #print('Peak ' + str(i) + ': ', end='')
+            #print 'Peak1 ' + str(i) + ': '
+            #print(peak1.frequency)
+
+            if peak1.assignment.find('?') == -1:
+                continue    # skip if already assigned
+
+            if ((peak1.frequency[-1] > exclude_from) and (peak1.frequency[-1] < exclude_to)):
+                continue
+
+            match_flag = 0
+            for j, peak2 in enumerate(self.specs_peaks[s2]):
+
+                #print 'Peak2 ' + str(j) + ': '
+                #print(peak2.frequency)
+
+                if peak2.assignment.find('?') == -1:
+                    continue    # skip if already assigned
+
+                if ((peak2.frequency[-1] > exclude_from) and (peak2.frequency[-1] < exclude_to)):
+                    continue
+
+                if abs(peak1.frequency[0] - peak2.frequency[0]) < tol:
+                    if abs(peak1.frequency[-1] - peak2.frequency[-1]) < tol_H:
+
+                        print("\nMatch:")
+                        print(peak1.frequency)
+                        print(peak2.frequency)
+                        match_flag = 1
+                        peak1.note += self.specs_names[s2] + ','
+                        peak2.note += self.specs_names[s1] + ','
+                        break
+
+            if match_flag == 0:
+                print('\n' + '*' * 20 + '\nNo match:')
+                print(peak1.frequency)
+
+        print('_' * 50)
 
 
-    for i in range(len(self.specs_nuclei)):
-        print self.specs_names[i]
-        print self.specs_nuclei[i]
+# Update the Note to count the frequency
+    for spec in self.specs_peaks:
+        for peak in spec:
+                xcheck_str_start = peak.note.find('xcheck:')
 
+                main_note = ''
+                if xcheck_str_start > 0:
+                    main_note = peak.note[ : xcheck_str_start] + ';'
 
+                xcheck = peak.note[xcheck_str_start + len('xcheck:') : ].strip(',')
+                if xcheck == '':
+                    new_note = main_note + 'xcheck:0'
 
-        #for peak in spec_peaks:
+                else:
+                    xcheck_array = xcheck.split(',')
+                    new_note = main_note + 'xcheck:'
 
-            #if peak.assignment.find('?') == -1:     # skip if already assigned
-             #   continue
+                    for (exp, freq) in collections.Counter(xcheck_array).items():
+                            new_note += exp + ':' + str(freq) + ','
 
-            #print peak.frequency[0]
-            #peak.note
+                peak.note = new_note.strip(',')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    self.status.config(text="Status: Done!")
+    self.status.update()
+    tkMessageBox.showinfo(title='Done!', message='Now check the Peak List (lt) for each experiment and see the Note property.')
 
 
 # ---------------------------------------------------------------------------
